84decb205afa4e185a1582da41ab16896f4a8ae2
Luke Canavan <lcanavan@continuum.io>
2017-08-14 17:26:27 -0500

    Initial working proof-of-concept

--- src/index.ts
+++ src/index.ts
@@ -6,39 +6,36 @@ import {
   Widget
 } from '@phosphor/widgets';
 
+import {
+  Message
+} from '@phosphor/messaging'
 
 /**
  * The MIME type for BokehJS.
  */
 export
-const BOKEHJS_MIME_TYPE = 'application/vnd.bokeh.v0+json';
+const BOKEHJS_LOAD_MIME_TYPE = 'application/vnd.bokehjs_load.v0';
+const BOKEHJS_EXEC_MIME_TYPE = 'application/vnd.bokehjs_exec.v0';
 
 /**
- * Load BokehJS into window
+ * Load BokehJS into dom
  */
 export
-class BokehJSModuleLoader extends Widget implements IRenderMime.IRenderer {
-
+class BokehJSLoad extends Widget implements IRenderMime.IRenderer {
 
   /**
    * Create a new widget
    */
   constructor(options: IRenderMime.IRendererOptions) {
     super();
-
     this._mimetype = options.mimeType;
   }
 
-  // where is `Message` defined?
-  // processMessage(msg: Message) {
-  //   ;
-  // }
-
   renderModel(model: IRenderMime.IMimeModel): Promise<void> {
-    // let payload = model.data[this._mimetype]
-    //
-    // do the things
-    //
+    let payload = model.data[this._mimetype] as string
+    const load_func = new Function(payload)
+    load_func()
+
     return Promise.resolve()
   }
 
@@ -46,20 +43,71 @@ class BokehJSModuleLoader extends Widget implements IRenderMime.IRenderer {
 }
 
 /**
+ * Exec BokehJS into window
+ */
+export
+class BokehJSExec extends Widget implements IRenderMime.IRenderer {
+
+  /**
+   * Create a new widget
+   */
+  constructor(options: IRenderMime.IRendererOptions) {
+    super();
+    this._mimetype = options.mimeType;
+    this.div = document.createElement('div');
+  }
+
+  renderModel(model: IRenderMime.IMimeModel): Promise<void> {
+    // this loads a div and script that renders the bokeh document and replaces the div
+    const payload = model.data[this._mimetype] as {script: string, div: string}
+    this.div.innerHTML = payload["div"]
+    this.node.appendChild(this.div)
+    this.script = new Function(payload["script"])
+
+    return Promise.resolve()
+  }
+
+  onAfterAttach(msg: Message): void {
+    this.script()
+  }
+
+
+  private _mimetype: string;
+  div: any;
+  script: Function;
+}
+
+/**
  * A mime renderer factory for Bokeh
  */
 export
-const rendererFactory: IRenderMime.IRendererFactory = {
+const loadRendererFactory: IRenderMime.IRendererFactory = {
   safe: true,
-  mimeTypes: [BOKEHJS_MIME_TYPE],
-  createRenderer: options => new BokehJSModuleLoader(options)
+  mimeTypes: [BOKEHJS_LOAD_MIME_TYPE],
+  createRenderer: options => new BokehJSLoad(options)
 }
 
-const extension: IRenderMime.IExtension = {
-  name: 'bokeh',
-  rendererFactory,
-  rank: 1, //needs to higher than application/javascript
-  dataType: 'json' // probably won't be json
+export
+const execRendererFactory: IRenderMime.IRendererFactory = {
+  safe: true,
+  mimeTypes: [BOKEHJS_EXEC_MIME_TYPE],
+  createRenderer: options => new BokehJSExec(options)
 }
 
-export default extension;
+
+const extensions: IRenderMime.IExtension[] = [
+  {
+    name: 'bokeh_load',
+    rendererFactory: loadRendererFactory,
+    rank: 0,
+    dataType: 'string'
+  },
+  {
+    name: 'bokeh_exec',
+    rendererFactory: execRendererFactory,
+    rank: 0,
+    dataType: 'json' // {"script": script, "div": div}
+  }
+]
+
+export default extensions;
