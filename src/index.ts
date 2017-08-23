97102df415fc62e682cd92fa6021c3b51b8a77b8
Luke Canavan <lcanavan@continuum.io>
2017-08-23 11:11:42 -0500

    Enable bokeh server docs

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,15 @@ import {
   Widget
 } from '@phosphor/widgets';
 
+import {
+  ReadonlyJSONObject
+} from '@phosphor/coreutils'
 
 /**
  * The MIME type for BokehJS.
  */
-const BOKEHJS_LOAD_MIME_TYPE = 'application/vnd.bokehjs_load.v0';
-const BOKEHJS_EXEC_MIME_TYPE = 'application/vnd.bokehjs_exec.v0';
-
+const BOKEHJS_LOAD_MIME_TYPE = 'application/vnd.bokehjs_load.v0+json';
+const BOKEHJS_EXEC_MIME_TYPE = 'application/vnd.bokehjs_exec.v0+json';
 
 /**
  * Load BokehJS into dom
@@ -26,10 +28,16 @@ class BokehJSLoad extends Widget implements IRenderMime.IRenderer {
   constructor(options: IRenderMime.IRendererOptions) {
     super();
     this._script_element = document.createElement("script")
+    this._div_element = document.createElement("div")
   }
 
   renderModel(model: IRenderMime.IMimeModel): Promise<void> {
-    this._script_element.textContent = model.data[this._load_mimetype] as string | ""
+    let data = model.data[this._load_mimetype] as ReadonlyJSONObject
+
+    this._div_element.innerHTML = data.div as string | ""
+    this._script_element.textContent = data.script as string | ""
+
+    this.node.appendChild(this._div_element)
     this.node.appendChild(this._script_element)
 
     return Promise.resolve()
@@ -37,11 +45,12 @@ class BokehJSLoad extends Widget implements IRenderMime.IRenderer {
 
   private _load_mimetype: string = BOKEHJS_LOAD_MIME_TYPE;
   private _script_element: HTMLScriptElement;
+  private _div_element: HTMLDivElement;
 }
 
 
 /**
- * Exec BokehJS into window
+ * Exec BokehJS in window
  */
 export
 class BokehJSExec extends Widget implements IRenderMime.IRenderer {
@@ -56,12 +65,25 @@ class BokehJSExec extends Widget implements IRenderMime.IRenderer {
   }
 
   renderModel(model: IRenderMime.IMimeModel): Promise<void> {
-    // this loads a div and script that renders the bokeh document and replaces the div
-    const payload = model.data[this._exec_mimetype] as {script: string, div: string} | {"script": "", "div": ""}
-    this._script_element.textContent = payload["script"]
-    this._div_element.innerHTML = payload["div"]
-    this.node.appendChild(this._script_element)
+    let data = model.data[this._exec_mimetype] as ReadonlyJSONObject
+    let metadata = model.metadata[this._exec_mimetype] as ReadonlyJSONObject
+
+    if (metadata.id !== undefined) {
+      // I'm a static document
+      this._div_element.innerHTML = data.div as string | ""
+      this._script_element.textContent = data.script as string | ""
+    } else if (metadata.server_id !== undefined) {
+      // I'm a server document
+      const ss = document.createElement('div');
+      ss.innerHTML = data.div as string | ""
+      const script_attrs : NamedNodeMap = ss.children[0].attributes
+      for (let key in script_attrs) {
+        this._script_element.setAttribute(script_attrs[key].name, script_attrs[key].value)
+      }
+    }
+
     this.node.appendChild(this._div_element)
+    this.node.appendChild(this._script_element)
 
     return Promise.resolve()
   }
