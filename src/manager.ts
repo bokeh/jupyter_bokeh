231b48eacc9d5a4dba6d71cfbf0554d2d1c93e05
Luke Canavan <lcanavan@anaconda.com>
2018-01-16 20:07:02 -0600

    Refactor to application extension to enable accessing doc context for server session cleanup

---
+++ src/manager.ts
@@ -0,0 +1,37 @@
+import {
+  IDisposable
+} from '@phosphor/disposable';
+
+import {
+  DocumentRegistry
+} from '@jupyterlab/docregistry';
+
+
+/**
+ * A micro manager that contains the document context
+ *
+ * This will grow in the future if we implement bokeh.io.push_notebook
+ */
+export
+class ContextManager implements IDisposable {
+  private _context: DocumentRegistry.IContext<DocumentRegistry.IModel>;
+
+  constructor(context: DocumentRegistry.IContext<DocumentRegistry.IModel>) {
+    this._context = context;
+  }
+
+  get context() {
+    return this._context;
+  }
+
+  get isDisposed(): boolean {
+    return this._context === null;
+  }
+
+  dispose(): void {
+    if (this.isDisposed) {
+      return;
+    }
+    this._context = null;
+  }
+}
---
+++ src/renderer.ts
@@ -0,0 +1,111 @@
+import {
+  IRenderMime
+} from '@jupyterlab/rendermime-interfaces'
+
+import {
+  KernelMessage
+} from '@jupyterlab/services'
+
+import {
+  ReadonlyJSONObject
+} from '@phosphor/coreutils'
+
+import {
+  Widget
+} from '@phosphor/widgets'
+
+import {
+  ContextManager
+} from './manager';
+
+
+/**
+ * The MIME types for BokehJS.
+ */
+const HTML_MIME_TYPE = 'text/html'
+const JS_MIME_TYPE = 'application/javascript'
+export const BOKEHJS_LOAD_MIME_TYPE = 'application/vnd.bokehjs_load.v0+json'
+export const BOKEHJS_EXEC_MIME_TYPE = 'application/vnd.bokehjs_exec.v0+json'
+
+/**
+ * Load BokehJS and CSS into the DOM
+ */
+export
+class BokehJSLoad extends Widget implements IRenderMime.IRenderer {
+  private _load_mimetype: string = BOKEHJS_LOAD_MIME_TYPE
+  private _script_element: HTMLScriptElement
+
+  constructor(options: IRenderMime.IRendererOptions) {
+    super()
+    this._script_element = document.createElement("script")
+  }
+
+  renderModel(model: IRenderMime.IMimeModel): Promise<void> {
+    let data = model.data[this._load_mimetype] as string
+    this._script_element.textContent = data
+    this.node.appendChild(this._script_element)
+
+    return Promise.resolve()
+  }
+}
+
+
+/**
+ * Exec BokehJS in window
+ */
+export
+class BokehJSExec extends Widget implements IRenderMime.IRenderer {
+  // for classic nb compat reasons, the payload in contained in these mime messages
+  private _html_mimetype: string = HTML_MIME_TYPE
+  private _js_mimetype: string = JS_MIME_TYPE
+  // the metadata is stored here
+  private _exec_mimetype: string = BOKEHJS_EXEC_MIME_TYPE
+  private _script_element: HTMLScriptElement
+  private _server_id: string
+  private _manager: ContextManager;
+
+  constructor(options: IRenderMime.IRendererOptions, manager: ContextManager) {
+    super()
+    this._script_element = document.createElement("script")
+    this._manager = manager
+  }
+
+  get isDisposed(): boolean {
+    return this._manager === null;
+  }
+
+  renderModel(model: IRenderMime.IMimeModel): Promise<void> {
+    let metadata = model.metadata[this._exec_mimetype] as ReadonlyJSONObject
+
+    if (metadata.id !== undefined) {
+      // I'm a static document
+      let data = model.data[this._js_mimetype] as string
+      this._script_element.textContent = data
+    } else if (metadata.server_id !== undefined) {
+      // I'm a server document
+      this._server_id = metadata.server_id as string
+      let data = model.data[this._html_mimetype] as string
+      const d = document.createElement('div')
+      d.innerHTML = data
+      const script_attrs: NamedNodeMap = d.children[0].attributes
+      for (let i in script_attrs) {
+        this._script_element.setAttribute(script_attrs[i].name, script_attrs[i].value)
+      }
+    }
+
+    this.node.appendChild(this._script_element)
+
+    return Promise.resolve()
+  }
+
+  dispose(): void {
+    if (this.isDisposed) {
+      return;
+    }
+    let content: KernelMessage.IExecuteRequest = {
+      code: `import bokeh.io.notebook as ion; ion.destroy_server('${this._server_id}')`
+    }
+    this._manager.context.session.kernel.requestExecute(content, true)
+    this._manager, this._server_id = null, null;
+  }
+}
