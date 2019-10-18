424a03a79a462867e6252963b46bf9f55e0e9a97
Mateusz Paprocki <mattpap@gmail.com>
2019-10-18 02:55:25 +0200

    Add a bundle for static embedding

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require("path")
+const {version} = require("./package.json")
+
+const externals = [/^@jupyterlab\/.+$/]
 
 module.exports = [
   /**
@@ -15,6 +18,29 @@ module.exports = [
       libraryTarget: "amd"
     },
     devtool: "source-map",
-    externals: [/^@jupyterlab\/.+$/],
+    externals,
+  },
+
+  /**
+   * Embeddable bundle
+   *
+   * This bundle is almost identical to the notebook extension bundle. The only
+   * difference is in the configuration of the webpack public path for the
+   * static assets.
+   *
+   * The target bundle is always `dist/index.js`, which is the path required by
+   * the custom widget embedder.
+   */
+  {
+    entry: "./lib/index.js",
+    output: {
+      filename: "index.js",
+      path: path.resolve(__dirname, "dist"),
+      libraryTarget: "amd",
+      library: "jupyterlab_bokeh",
+      publicPath: "https://unpkg.com/jupyterlab_bokeh@" + version + "/dist/"
+    },
+    devtool: "source-map",
+    externals,
   },
 ]
