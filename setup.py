548d33532ac23bd18d3e8eb4f4f6398069715e2b
Mateusz Paprocki <mattpap@gmail.com>
2020-03-09 22:10:34 +0100

    Update version to 2.0.0

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ install_requires = [
 
 setup_args = dict(
     name="jupyter_bokeh",
-    version="1.2.0",
+    version="2.0.0",
     install_requires=install_requires,
     description="A Jupyter extension for rendering Bokeh content.",
     long_description=open("README.md").read(),
