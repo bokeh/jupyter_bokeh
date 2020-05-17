80232f174bc1b70b4fb9f4d53135c5221d666155
Philipp Rudiger <prudiger@anaconda.com>
2020-05-17 15:49:25 +0200

    Handle other events

--- src/widgets.ts
+++ src/widgets.ts
@@ -27,6 +27,42 @@ export type RenderBundle = {
   div: string
 }
 
+export interface DocumentChanged {
+  event: "jsevent"
+  kind: string
+}
+
+export interface ModelChanged extends DocumentChanged {
+  event: "jsevent"
+  kind: "ModelChanged"
+  id: string
+  new: unknown
+  attr: string
+  old: unknown
+  hint: unknown
+}
+
+export interface MessageSent extends DocumentChanged {
+  event: "jsevent"
+  kind: "MessageSent"
+  msg_data: {
+    event_name: string,
+    event_values: {
+      model: {id: string},
+      [other: string]: any
+    }
+  }
+  msg_type: string
+}
+
+function isModelChanged(msg: DocumentChanged): msg is ModelChanged {
+  return (msg as ModelChanged).kind === "ModelChanged"
+}
+
+function isMessageSent(msg: DocumentChanged): msg is MessageSent {
+  return (msg as MessageSent).kind === "MessageSent"
+}
+
 export class BokehModel extends DOMWidgetModel {
   defaults() {
     return {
@@ -56,26 +92,32 @@ export class BokehView extends DOMWidgetView {
   private _blocked: boolean
   private _msgs: any[]
   private _idle: boolean
+  private _combine: boolean
 
   constructor(options?: any) {
     super(options)
     this._document = null
     this._blocked = false
     this._idle = true
+    this._combine = true
     this._msgs = []
     const {Receiver} = bk_require("protocol/receiver")
     this._receiver = new Receiver()
     this.model.on("change:render_bundle", () => this.render())
     if ((window as any).Jupyter != null && (window as any).Jupyter.notebook != null) {
-	  // Handle classic Jupyter notebook
+      // Handle classic Jupyter notebook
       const events = (window as any).require('base/js/events')
       events.on('kernel_idle.Kernel', () => this._process_msg())
     } else if ((this.model.widget_manager as any).context != null) {
-	  // Handle JupyterLab
+      // Handle JupyterLab
       (this.model.widget_manager as any).context.sessionContext.statusChanged.connect((_: any, status: any) => {
         if (status === "idle")
           this._process_msg()
       })
+    } else {
+      if (this.model.get("combine_events"))
+		console.warn("BokehView cannot combine events because Kernel idle status cannot be determined.")
+      this._combine = false
     }
     this.listenTo(this.model, "msg:custom", (content, buffers) => this._consume_patch(content, buffers))
   }
@@ -106,23 +148,77 @@ export class BokehView extends DOMWidgetView {
     this._document.on_change((event: any) => this._change_event(event))
   }
 
-  protected _change_event(event: DocumentChangedEvent): void {
-    const {ModelChangedEvent} = bk_require("document/events")
-    if (!this._blocked && event instanceof ModelChangedEvent) {
-      if (!this._idle && this.model.get("combine_events")) {
-        // Queue event and drop previous events on same model attribute
-        const new_msgs = []
-        for (const msg of this._msgs) {
-          if ((msg.id != event.model.id) || (msg.attr != event.attr))
-            new_msgs.push(msg)
-        }
-        new_msgs.push({event: "jsevent", id: event.model.id, new: event.new_, attr: event.attr, old: event.old})
-        this._msgs = new_msgs
-      } else {
-        this._idle = false
-        this.send({event: "jsevent", id: event.model.id, new: event.new_, attr: event.attr, old: event.old})
+  _combine_events(new_msg: any): DocumentChanged[] {
+	const new_msgs = []
+    for (const msg of this._msgs) {
+      if (new_msg.kind != msg.kind)
+		new_msgs.push(msg)
+	  else if (isModelChanged(msg) && isModelChanged(new_msg)) {
+       if (msg.id != new_msg.id || msg.attr != new_msg.attr)
+          new_msgs.push(msg)
+      } else if (isMessageSent(msg) && isMessageSent(new_msg)) {
+        if (
+		  msg.msg_data.event_values.model.id != new_msg.msg_data.event_values.model.id ||
+		  msg.msg_data.event_name != new_msg.msg_data.event_name
+		)
+          new_msgs.push(msg)
       }
     }
+    new_msgs.push(new_msg)
+	return new_msgs
+  }
+
+  _send(msg: DocumentChanged): void {
+	if (!this._idle && this._combine && this.model.get("combine_events"))
+      // Queue event and drop previous events on same model attribute
+      this._msgs = this._combine_events(msg)
+	else {
+      this._idle = false
+      this.send(msg)
+    }
+  }
+
+  protected _change_event(event: DocumentChangedEvent): void {
+    if (this._blocked) { return }
+    const {ModelChangedEvent, MessageSentEvent} = bk_require("document/events")
+    if (event instanceof ModelChangedEvent) {
+      let js_msg: ModelChanged = {
+        event: "jsevent",
+        kind: "ModelChanged",
+        id: event.model.id,
+        attr: event.attr,
+        new: event.new_,
+        old: event.old,
+        hint: null
+	  }
+      if (event.hint != null) {
+        if (event.hint.patches != null) {
+          js_msg["hint"] = {
+            column_source: event.hint.column_source,
+            patches: event.hint.patches
+          }  
+        } else if (event.hint.data != null) {
+          js_msg["hint"] = {
+            column_source: event.hint.column_source,
+            data: event.hint.data,
+            rollover: event.hint.rollover
+          }
+        }
+      }
+	  this._send(js_msg)
+	} else if ((event instanceof MessageSentEvent) && (event.msg_type == "bokeh_event")) {
+	  const msg_data = {...event.msg_data}
+	  const event_values = {...msg_data.event_values}
+	  event_values["model"] = {id: event_values.model.id}
+      msg_data["event_values"] = event_values
+      let js_msg: MessageSent = {
+        event: "jsevent",
+        kind: "MessageSent",
+        msg_type: event.msg_type,
+        msg_data: msg_data
+      }
+	  this._send(js_msg)
+	}
   }
 
   protected _consume_patch(content: {msg: "patch", payload?: Fragment}, buffers: DataView[]): void {
