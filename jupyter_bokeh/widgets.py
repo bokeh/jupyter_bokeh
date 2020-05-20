0baebe6a21de652f9a9c8d7038372ed0749ed90f
Mateusz Paprocki <mattpap@gmail.com>
2020-05-20 16:43:20 +0200

    jupyter_bokeh 2.0.2

--- jupyter_bokeh/widgets.py
+++ jupyter_bokeh/widgets.py
@@ -40,7 +40,7 @@ __all__ = (
 )
 
 _module_name = "@bokeh/jupyter_bokeh"
-_module_version = "^2.0.1"
+_module_version = "^2.0.2"
 
 #-----------------------------------------------------------------------------
 # General API
