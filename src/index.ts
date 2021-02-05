c9569022716d8128952a262f18adf650f3426a10
Philipp Rudiger <prudiger@anaconda.com>
2021-02-05 19:09:08 +0100

    Run JLab2 extension migration

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,17 @@
-import {extension} from "./plugin"
-export default extension
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
 
-export * from "./widgets"
+/**
+ * Initialization data for the @bokeh/jupyter_bokeh extension.
+ */
+const extension: JupyterFrontEndPlugin<void> = {
+  id: '@bokeh/jupyter_bokeh:plugin',
+  autoStart: true,
+  activate: (app: JupyterFrontEnd) => {
+    console.log('JupyterLab extension @bokeh/jupyter_bokeh is activated!');
+  }
+};
+
+export default extension;
