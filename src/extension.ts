60dffcd0ab95a0f6c795dc3004c184733324f630
Philipp Rudiger <prudiger@anaconda.com>
2021-03-21 15:36:04 +0100

    Lint fix

--- src/extension.ts
+++ src/extension.ts
@@ -5,6 +5,6 @@
 // Some static assets may be required by the custom widget javascript. The base
 // url for the notebook is not known at build time and is therefore computed
 // dynamically.
-(window as any).__webpack_public_path__ = document.querySelector("body")!.getAttribute("data-base-url") + "nbextensions/jupyter_bokeh"
+(window as any).__webpack_public_path__ = document.querySelector('body')!.getAttribute('data-base-url') + 'nbextensions/jupyter_bokeh'
 
-export * from "./index"
+export * from './index'
