d844848529283096c9b7c969aea9910ff810cd2f
Mateusz Paprocki <mattpap@gmail.com>
2023-06-25 11:21:56 +0200

    jupyter_bokeh 3.0.8

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,11 +4,12 @@ set -e
 set -x
 
 git clean -dfx
-npm install
+jlpm install
+jlpm run build:prod
 npm publish --access public # token is configured in ~/.npmrc; requires OTP
 
 git clean -dfx
-python setup.py sdist
+python setup.py sdist bdist_wheel
 twine upload -u __token__ -p $(cat ~/.tokens/pypi_jupyter_bokeh) dist/jupyter_bokeh-*.tar.gz
 
 git clean -dfx
