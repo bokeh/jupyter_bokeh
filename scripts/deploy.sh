475f4e96f9ea4ce46b10c35119cfdd1cbeb523ab
Mateusz Paprocki <mattpap@gmail.com>
2026-06-09 13:14:54 +0200

    Update deployment instructions

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,12 +6,13 @@ set -x
 git clean -dfx
 jlpm install
 jlpm run build:prod
-npm publish --access public # token is configured in ~/.npmrc; requires OTP
+npm login
+npm publish --access public
 
 git clean -dfx
 hatch build
 hatch publish -u __token__ -a $(cat ~/.tokens/pypi_jupyter_bokeh)
 
 VERSION=`hatch version` conda build conda.recipe
-PKG=$(conda build conda.recipe --output)
+PKG=$(VERSION=`hatch version` conda build conda.recipe --output)
 anaconda --token ~/.tokens/anaconda upload -u bokeh -l dev -l main $PKG
